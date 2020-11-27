51da0546ea3bad53ae3f0c68ddc908634b30d9c0
evergreen2001 <dicksonevergreenido@gmail.com>
2020-11-28 00:41:10 +0100

    grating a get request

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Recipes from "../src/Recipes";
 
 function App() {
+  const APP_ID = "7eacc5e2";
+  const API_KEY = "9a645d8682cce8be15d64b6eef45bccd";
 
-  const App_ID = '7eacc5e2'
-  const API_KEY = '9a645d8682cce8be15d64b6eef45bccd' ;
+  const [recipes, setRecipes] = useState([]);
+  const EndPoint = ``;
 
-  const request = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}"
-  
+  useEffect(() => {
+    getRecipes();
+  }, []);
 
+  const getRecipes = async () => {
+    const res = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`
+    );
+    const data = await res.json();
+
+    console.log(data.hits);
+
+    setRecipes(data.hits);
+  };
   return (
     <div className="App">
-    <form>
+      <form>
+        <input type="text" name="" id="" className="input__search" />
+        <button type="submit" className="btn"></button>
+      </form>
 
-
-
-    <input type="text" name="" id="" className="input__search"/>
-    <button type="submit" className="btn">
-      Submit
-    </button>
-
-
-
-    </form>
+      {recipes.map((recipe) => (
+        <Recipes title={recipe.recipe.label} calo={recipe.recipe.calories} image={recipe.recipe.image} />
+      ))}
     </div>
   );
 }
---
+++ src/Recipes.js
@@ -0,0 +1,16 @@
+// import React, { Component } from "react";
+
+function Recipes() {
+  return (
+    <div>
+      <h1>Title</h1>
+
+      <p>Calo</p>
+
+      <img src="" alt="" />
+    </div>
+  );
+}
+
+
+export default Recipes
