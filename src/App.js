999bb36cc7d3efc33e984338ee96e6d95874f241
evergreen2001 <dicksonevergreenido@gmail.com>
2020-11-27 23:12:13 +0100

    adding input files

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import './App.css';
 
 function App() {
 
-  const App_ID = ''
+  const App_ID = '7eacc5e2'
   const API_KEY = '9a645d8682cce8be15d64b6eef45bccd' ;
+
+  const request = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}"
+  
 
   return (
     <div className="App">
-    jj
+    <form>
+
+
+
+    <input type="text" name="" id="" className="input__search"/>
+    <button type="submit" className="btn">
+      Submit
+    </button>
+
+
+
+    </form>
     </div>
   );
 }
