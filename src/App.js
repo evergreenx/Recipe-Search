c3fa8871d2eadfaecb4454fa7f3c70c7700d9df7
evergreen2001 <dicksonevergreenido@gmail.com>
2020-11-27 22:41:37 +0100

    cleaning up files

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const App_ID = ''
+  const API_KEY = '9a645d8682cce8be15d64b6eef45bccd' ;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    jj
     </div>
   );
 }
