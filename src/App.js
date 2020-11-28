d62162858a3eadd8840baabdc99c60f4cc533b17
evergreen2001 <dicksonevergreenido@gmail.com>
2020-11-28 11:24:23 +0100

    styling cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 import Recipes from "../src/Recipes";
@@ -9,16 +9,17 @@ function App() {
   const API_KEY = "9a645d8682cce8be15d64b6eef45bccd";
 
   const [recipes, setRecipes] = useState([]);
- const [search, setSearch] = useState('')
-  const EndPoint = ``;
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+  // const EndPoint = ``;
 
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
     const res = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${API_KEY}`
     );
     const data = await res.json();
 
@@ -27,39 +28,57 @@ function App() {
     setRecipes(data.hits);
   };
 
-
   const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
 
-    setSearch(e.target.value )
-    console.log(e.target.value )
-    
-  }
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
   return (
-    <div className="App bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <form>
-      <div class="font-sans text-black mb-3 flex items-center justify-center">
-  <div class="border rounded overflow-hidden flex">
-    <input type="text" value={search} onChange={updateSearch} class="px-4 py-2" placeholder="Search..."/>
-    <button class="flex items-center justify-center px-4 border-l">
-      <svg class="h-4 w-4 text-grey-dark" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg>
-    </button>
-  
-</div>
-</div>
-      
+    <div className="bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <form onSubmit={getSearch}>
+        <div className="font-sans text-black mb-3 flex items-center justify-center">
+          <div className="border rounded overflow-hidden flex">
+            <input
+              type="text"
+              value={search}
+              onChange={updateSearch}
+              className="px-4 py-2"
+              placeholder="Search..."
+            />
+            <button
+              className="flex items-center justify-center px-4 border-l"
+              type="submit"
+            >
+              <svg
+                className="h-4 w-4 text-grey-dark"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+              </svg>
+            </button>
+          </div>
+        </div>
       </form>
 
-      <div class="flex flex-wrap -mx-1 lg:-mx-4">
-
-      {recipes.map((recipe) => (
-
-
-        <Recipes key={recipe.recipe.label} title={recipe.recipe.label} calo={recipe.recipe.calories} image={recipe.recipe.image} />
-
-      ))}
-
+      <div className="flex flex-wrap -mx-1 lg:-mx-4">
+        {recipes.map((recipe) => (
+          <Recipes
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calo={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
       </div>
 
+      <img src="../api.jpeg" alt="edamam" />
     </div>
   );
 }
--- src/Recipes.js
+++ src/Recipes.js
@@ -1,36 +1,33 @@
 // import React, { Component } from "react";
 
-function Recipes({ title, calo, image }) {
+function Recipes({ title, calo, image, ingredients }) {
   return (
-   
-    
-        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-        
-          <article className="overflow-hidden rounded-lg shadow-lg">
-            <a href="#">
-              <img
-                alt="Placeholder"
-                class="block h-auto w-full"
-                src={image}              />
+    <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+      <article className="overflow-hidden rounded-lg shadow-lg">
+        <a href="#">
+          <img alt="Placeholder" className="block h-auto w-full" src={image} />
+        </a>
+
+        <header className="p-2 md:p-4">
+          <h1 className="text-lg">
+            <a className="no-underline hover:underline text-black" href="#">
+              {title}
             </a>
+          </h1>
+        <p>Calories : {calo}</p>
 
-            <header class="flex items-center justify-between leading-tight p-2 md:p-4">
-              <h1 class="text-lg">
-                <a class="no-underline hover:underline text-black" href="#">
-                  {title}
-                </a>
-              </h1>
-              
-              <p>{calo}</p>
-            </header>
+        </header>
 
-           
-          </article>
-        
-        </div>
-    
+        {ingredients.map((ingredient) => (
+            <ul className="p-2">
+            <li>{ingredient.text}</li>
 
-  
+            </ul>
+        ))}
+
+        {}
+      </article>
+    </div>
   );
 }
 
