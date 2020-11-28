21e3d6f4e86f55c1c18ef860b7abb75e65c5357d
evergreen2001 <dicksonevergreenido@gmail.com>
2020-11-28 22:55:30 +0100

    fixing error

--- src/Recipes.js
+++ src/Recipes.js
@@ -4,13 +4,13 @@ function Recipes({ title, calo, image, ingredients }) {
   return (
     <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
       <article className="overflow-hidden rounded-lg shadow-lg">
-        <a href="#">
+       
           <img alt="Placeholder" className="block h-auto w-full" src={image} />
-        </a>
+      
 
         <header className="p-2 md:p-4">
           <h1>
-            <a className="no-underline hover:underline text-black" href="#">
+            <a className="no-underline hover:underline text-black" href="/">
               {title}
             </a>
           </h1>
