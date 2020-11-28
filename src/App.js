0d8cbe695d85951facc55483768735021efe40a2
evergreen2001 <dicksonevergreenido@gmail.com>
2020-11-28 02:27:50 +0100

    added tailwind css component

--- src/App.js
+++ src/App.js
@@ -26,15 +26,31 @@ function App() {
     setRecipes(data.hits);
   };
   return (
-    <div className="App">
+    <div className="App bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <form>
-        <input type="text" name="" id="" className="input__search" />
-        <button type="submit" className="btn"></button>
+      <div class="font-sans text-black mb-3 flex items-center justify-center">
+  <div class="border rounded overflow-hidden flex">
+    <input type="text" class="px-4 py-2" placeholder="Search..."/>
+    <button class="flex items-center justify-center px-4 border-l">
+      <svg class="h-4 w-4 text-grey-dark" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg>
+    </button>
+  
+</div>
+</div>
+      
       </form>
 
+      <div class="flex flex-wrap -mx-1 lg:-mx-4">
+
       {recipes.map((recipe) => (
-        <Recipes title={recipe.recipe.label} calo={recipe.recipe.calories} image={recipe.recipe.image} />
+
+
+        <Recipes key={recipe.recipe.label} title={recipe.recipe.label} calo={recipe.recipe.calories} image={recipe.recipe.image} />
+
       ))}
+
+      </div>
+
     </div>
   );
 }
--- src/Recipes.js
+++ src/Recipes.js
@@ -1,16 +1,37 @@
 // import React, { Component } from "react";
 
-function Recipes() {
+function Recipes({ title, calo, image }) {
   return (
-    <div>
-      <h1>Title</h1>
+   
+    
+        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+        
+          <article className="overflow-hidden rounded-lg shadow-lg">
+            <a href="#">
+              <img
+                alt="Placeholder"
+                class="block h-auto w-full"
+                src={image}              />
+            </a>
 
-      <p>Calo</p>
+            <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+              <h1 class="text-lg">
+                <a class="no-underline hover:underline text-black" href="#">
+                  {title}
+                </a>
+              </h1>
+              
+              <p>{calo}</p>
+            </header>
 
-      <img src="" alt="" />
-    </div>
+           
+          </article>
+        
+        </div>
+    
+
+  
   );
 }
 
-
-export default Recipes
+export default Recipes;
