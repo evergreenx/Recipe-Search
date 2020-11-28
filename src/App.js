a74f5a7ae6bedd098968b1c73ce5aade1420caba
evergreen2001 <dicksonevergreenido@gmail.com>
2020-11-28 09:00:03 +0100

    adding search functionality

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   const API_KEY = "9a645d8682cce8be15d64b6eef45bccd";
 
   const [recipes, setRecipes] = useState([]);
+ const [search, setSearch] = useState('')
   const EndPoint = ``;
 
   useEffect(() => {
@@ -25,12 +26,20 @@ function App() {
 
     setRecipes(data.hits);
   };
+
+
+  const updateSearch = (e) => {
+
+    setSearch(e.target.value )
+    console.log(e.target.value )
+    
+  }
   return (
     <div className="App bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <form>
       <div class="font-sans text-black mb-3 flex items-center justify-center">
   <div class="border rounded overflow-hidden flex">
-    <input type="text" class="px-4 py-2" placeholder="Search..."/>
+    <input type="text" value={search} onChange={updateSearch} class="px-4 py-2" placeholder="Search..."/>
     <button class="flex items-center justify-center px-4 border-l">
       <svg class="h-4 w-4 text-grey-dark" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg>
     </button>
